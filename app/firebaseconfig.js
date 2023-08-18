b1e60ef87f2e54109380dc1646eaaa6307359370
DarKS4ys <78923614+DarKS4ys@users.noreply.github.com>
2023-08-18 18:06:31 +0300

    Update firebaseconfig.js

--- app/firebaseconfig.js
+++ app/firebaseconfig.js
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDcItUIdWwLBcWu2bsl8BpxreZEiS9UTb0",
-  authDomain: "fir-firestore-779e8.firebaseapp.com",
-  projectId: "fir-firestore-779e8",
-  storageBucket: "fir-firestore-779e8.appspot.com",
-  messagingSenderId: "1003893961699",
-  appId: "1:1003893961699:web:436c71a36f78b2ae9fe6d6",
-  measurementId: "G-0LGC27H68W"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
